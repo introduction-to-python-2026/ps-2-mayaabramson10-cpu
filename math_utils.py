32242a198b5be16530c99e8317416e42857be0ec
mayaabramson10-cpu <maya.abramson10@gmail.com>
2025-10-29 17:24:30 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,5 +1,13 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    
+    if num1 >= num2 and num1 >= num3:
+      return num1
+
+    elif num2 >= num1 and num2 >= num3:
+      return num2
+
+    return num3
+
 
 def find_mean(num1, num2, num3):
     pass  # Replace 'pass' with code
