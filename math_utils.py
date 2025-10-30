83ce82b454f418c93b5f96afdcc7e24cd11cca3a
mayaabramson10-cpu <maya.abramson10@gmail.com>
2025-10-30 17:03:28 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,13 +1,9 @@
 def find_max_number(num1, num2, num3):
-    
     if num1 > num2 and num1 > num3:
       return num1
-
     elif num2 > num1 and num2 > num3:
       return num2
-
     return num3
-
 
 def find_mean(num1, num2, num3):
     mean = (num1 + num2 + num3)/3
